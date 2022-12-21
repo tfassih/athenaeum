13f8b507ff3788266d1d9bb74fab0cbe7419108b
THOMAS FASSIH <thomasfassih@gmail.com>
2022-12-20 21:42:43 -0600

    fixed render

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const {queryResult} = require("pg-promise");
+const {json} = require("express");
 const app = express();
 const pgp = require('pg-promise')();
 const db = pgp('postgres://postgres:password@localhost:5432/athenaeum')
@@ -8,6 +9,7 @@ const port = 3000;
 
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', './views');
@@ -21,8 +23,16 @@ app.get('/users', function(req, res) {
     db.result('SELECT * FROM users').then((result) => {
         users = result.rows;
         console.log(users);
-        res.render('users.ejs', {user: users});
-    });
+        res.get('user');
+
+        res.render('users.ejs', {user: res.send(users)});
+
+
+    }).catch(err => {
+        console.error(err);
+    }).finally(() => {
+        res.end();
+    })
 
 });
 
